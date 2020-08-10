b6298e04a05f33813ba539ed464fed552eef940a
Dwetzel <danjwetzel@gmail.com>
2020-08-09 22:33:37 -0700

    fixed errors

--- src/Controller/LogIn.java
+++ src/Controller/LogIn.java
@@ -7,11 +7,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
+import sun.rmi.runtime.Log;
 import sun.security.mscapi.CPublicKey;
 
 import java.io.*;
@@ -34,6 +37,12 @@ public class LogIn implements Initializable {
     @FXML
     private Button logInButton;
 
+    @FXML
+    private Label userNameLbl;
+
+    @FXML
+    private Label passwordLbl;
+
     public LogIn() throws SQLException {
     }
 
@@ -49,7 +58,7 @@ public class LogIn implements Initializable {
     static final String password = "53688672953";
     public static Connection conn;
 
-    static Locale locale = Locale.getDefault();
+    public static Locale locale = Locale.getDefault();
 
     public static String getUsername() {
         return username;
@@ -63,6 +72,19 @@ public class LogIn implements Initializable {
         }
     }
 
+    void alertConnected() {
+        Alert alertType = new Alert(Alert.AlertType.INFORMATION);
+        alertType.setTitle("Status");
+        alertType.setContentText("Connected to the database.");
+        alertType.showAndWait();
+    }
+
+    void alertConnectedEs() {
+        Alert alertType = new Alert(Alert.AlertType.INFORMATION);
+        alertType.setTitle("Estado");
+        alertType.setContentText("Conexión establecida");
+        alertType.showAndWait();
+    }
 
     @FXML
     public Connection goToCalendar(ActionEvent event) throws IOException {
@@ -70,7 +92,7 @@ public class LogIn implements Initializable {
             Class.forName(MYSQLJDBCDriver);
             conn = DriverManager.getConnection(jdbcURL, userField.getText(), passwordField.getText());
             if (locale.getLanguage().equals(new Locale("es").getLanguage())) {
-                System.out.println("\n" + "Conectado a la base de datos.");
+                alertConnectedEs();
                 try(FileWriter fw = new FileWriter("Log.txt", true);
                     BufferedWriter bw = new BufferedWriter(fw);
                     PrintWriter out = new PrintWriter(bw)) {
@@ -79,7 +101,7 @@ public class LogIn implements Initializable {
                 }
             } else if (locale.getLanguage().equals(new Locale("en").getLanguage())) {
                 System.out.println("Connected to the database.");
-
+                alertConnected();
                 try(FileWriter fw = new FileWriter("Log.txt", true);
                 BufferedWriter bw = new BufferedWriter(fw);
                 PrintWriter out = new PrintWriter(bw)) {
@@ -98,9 +120,19 @@ public class LogIn implements Initializable {
             window.setTitle("Calendar");
             window.show();
         } catch (ClassNotFoundException e) {
-            System.out.println(e.getLocalizedMessage());
+            Alert alertType = new Alert(Alert.AlertType.ERROR);
+            alertType.setTitle("Error");
+            alertType.setContentText("Error: " + e.getLocalizedMessage());
+            alertType.showAndWait();
         } catch (SQLException e) {
-            System.out.println("Error: " + e.getLocalizedMessage());
+            Alert alertType = new Alert(Alert.AlertType.ERROR);
+            alertType.setTitle("Error");
+            if (locale.getLanguage().equals(new Locale("es").getLanguage())) {
+                alertType.setContentText("Error: el nombre de usuario y la contraseña no coinciden.");
+            } else if (locale.getLanguage().equals(new Locale("en").getLanguage())) {
+                alertType.setContentText("Error: The username and password did not match.");
+            }
+            alertType.showAndWait();
         }
         return conn;
     }
@@ -108,12 +140,12 @@ public class LogIn implements Initializable {
     public static void closedConnection() {
         try {
             System.getProperty("user.country");
+            if (locale.getLanguage().equals(new Locale("es").getLanguage())) {
+                System.out.println("Desconectado");
+            } else if (locale.getLanguage().equals(new Locale("en").getLanguage())) {
+                System.out.println("Disconnected.");
             conn.close();
 
-            if (locale.getLanguage().equals(new Locale("es").getLanguage())) {
-                System.out.println("Desconectado de la base de datos.");
-            } else if (locale.getLanguage().equals(new Locale("en").getLanguage())) {
-                System.out.println("Disconnected from the database.");
             }
         } catch (SQLException e) {
             System.out.println("Error: " + e.getLocalizedMessage());
@@ -123,5 +155,10 @@ public class LogIn implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if (locale.getLanguage().equals(new Locale("es").getLanguage())) {
+            logInButton.setText("Iniciar sesión");
+            userNameLbl.setText("Nombre de usuario");
+            passwordLbl.setText("Contraseña");
+        }
     }
 }
